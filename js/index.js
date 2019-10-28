a0428d1c9b11c7b1d06de6ef84cdc8808d2d486e
mjedwards <jairedwards20@gmail.com>
2019-10-28 13:27:13 -0400

    first commit

---
+++ js/index.js
@@ -0,0 +1,22 @@
+let navbar = document.querySelector(".nav");
+let headerItems = document.querySelector(".header-items");
+
+function navAnimation() {
+  window.addEventListener("scroll", function() {
+    if (window.scrollY != 0) {
+      navbar.setAttribute(
+        "style",
+        "position:fixed; background: #197ba8; z-index: 4;"
+      );
+      headerItems.setAttribute("style", "margin-top: 266px;");
+    } else {
+      navbar.setAttribute(
+        "style",
+        "position:initial; background: none; z-index: 4"
+      );
+      headerItems.setAttribute("style", "margin-top: none;");
+    }
+  });
+}
+
+navAnimation();
